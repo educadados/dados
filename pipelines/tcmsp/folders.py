cfd49f85aa2833a0d18c74d14146c24fde21fe40
Edson Bomfim <edinhodiluviano@gmail.com>
2019-05-02 12:30:33 -0300

    modificadas pastas dos arquivos do tcm-sp

--- pipelines/tcmsp/folders.py
+++ pipelines/tcmsp/folders.py
@@ -6,4 +6,6 @@ import os
 
 BASE_FOLDER = os.path.split(os.environ['VIRTUAL_ENV'])[0]
 DOWNLOAD_FOLDER = os.path.join(BASE_FOLDER, 'raw_data/tcmsp')
+DOTACAO_FOLDER = os.path.join(DOWNLOAD_FOLDER, 'dotacao')
+EMPENHO_FOLDER = os.path.join(DOWNLOAD_FOLDER, 'empenho')
 CLEAN_FOLDER = os.path.join(BASE_FOLDER, 'clean')
